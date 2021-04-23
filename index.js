9c47c9dc26b20620a1cdbb35a41be0aef3900595
Luca Strebel <luca.strebel@jmc-software.ch>
2021-04-23 14:25:03 +0200

    beautify output

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function recursiveCall(index){
         if(response.statusCode == 200){
             console.log('FREE ' + possible[index]);
             fs.appendFile('free.txt', possible[index] + '\n', function(err) {
-                console.log(err);
+                err && console.log(err);
             });
-            
+
         } else if(response.statusCode != 202){
             console.log('ERROR ENCOUNTERD! EXITING');
         }
