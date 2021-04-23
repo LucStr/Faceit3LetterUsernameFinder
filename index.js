4c49add7d0586a7b10b71b15c2c5fe16a3214d91
Luca Strebel <luca.strebel@jmc-software.ch>
2021-04-23 14:19:12 +0200

    add functionality

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const request = require('request');
+const fs = require('fs');
 
+const validChars = [..."abcdefghijklmnopqrstuvwxzy0123456789_-"];
+
+const possible = [];
+
+validChars.forEach(a => {
+    validChars.forEach(b => {
+        validChars.forEach(c => {
+            possible.push(a + b + c);
+        });
+    });
+});
+
+function recursiveCall(index){
+    if(index % 100 == 0){
+        console.log('Reached ' + index);
+    }
+    if(index == possible.length){
+        console.log('DONE!')
+        return;
+    }
+
+    request('https://api.faceit.com/core/v1/nicknames?nickname=' + possible[index], function (error, response, body) {
+        if(response.statusCode == 200){
+            console.log('FREE ' + possible[index]);
+            fs.appendFile('free.txt', possible[index] + '\n', function(err) {
+                console.log(err);
+            });
+            
+        } else if(response.statusCode != 202){
+            console.log('ERROR ENCOUNTERD! EXITING');
+        }
+
+        recursiveCall(++index);
+    });
+}
+
+recursiveCall(0);
